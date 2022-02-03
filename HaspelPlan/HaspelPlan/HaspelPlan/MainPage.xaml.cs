66f6347ccd01440859f087413879088f1052857a
AHL <Ahlers@boehme-weihs.de>
2022-02-03 11:49:09 +0100

    Added initial source code

---
+++ HaspelPlan/HaspelPlan/HaspelPlan/MainPage.xaml.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace HaspelPlan
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            showPlan();
+        }
+
+        private string LoadHttpPageWithBasicAuthentication(string url, string username, string password)
+        {
+            string fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "cache.txt");
+            string pageContent = "";
+            bool isConnectionSuccessful = false;
+            Uri myUri = new Uri(url);
+            WebRequest myWebRequest = HttpWebRequest.Create(myUri);
+
+            HttpWebRequest myHttpWebRequest = (HttpWebRequest)myWebRequest;
+            myHttpWebRequest.UserAgent = "Mozilla/5.0 (Windows NT 6.3; WOW64; rv:54.0) Gecko/20100101 Firefox/54.0";
+            myHttpWebRequest.ContentType = "application/x-www-form-urlencoded";
+            myHttpWebRequest.AllowAutoRedirect = true;
+            myHttpWebRequest.Timeout = 100000;
+
+            NetworkCredential myNetworkCredential = new NetworkCredential(username, password);
+
+            CredentialCache myCredentialCache = new CredentialCache();
+            myCredentialCache.Add(myUri, "Basic", myNetworkCredential);
+
+            myHttpWebRequest.PreAuthenticate = true;
+            myHttpWebRequest.Credentials = myCredentialCache;
+
+            try
+            {
+                WebResponse myWebResponse = myWebRequest.GetResponse();
+                Stream responseStream = myWebResponse.GetResponseStream();
+
+                StreamReader myStreamReader = new StreamReader(responseStream, Encoding.Default);
+
+                pageContent = myStreamReader.ReadToEnd();
+
+                responseStream.Close();
+                myWebResponse.Close();
+                try
+                {
+                    File.WriteAllText(fileName, pageContent);
+                }
+                catch { }
+                isConnectionSuccessful = true;
+            }
+            catch
+            {
+                try
+                {
+                    if (File.Exists(fileName))
+                    {
+                        pageContent = File.ReadAllText(fileName);
+                    }
+                    else
+                    {
+                        noConnection.IsVisible = true;
+                        planView.IsVisible = false;
+                        isConnectionSuccessful = false;
+                    }
+                }
+                catch{}
+                    
+            }
+            if (isConnectionSuccessful)
+            {
+                noConnection.IsVisible = false;
+                planView.IsVisible = true;
+            }
+            return pageContent;
+        }
+        private void showPlan()
+        {
+            int calendarWeek = getCalendarWeek();
+            string content = LoadHttpPageWithBasicAuthentication($"http://www.bkah.de/schuelerplan_praesenz/{calendarWeek}/c/c00118.htm", "schuelerplan", "schwebebahn");
+            var html = new HtmlWebViewSource
+            {
+                Html = content
+            };
+            planView.Source = html;
+
+        }
+        private int getCalendarWeek()
+        {
+            CultureInfo myCI = new CultureInfo("de-de");
+            Calendar myCal = myCI.Calendar;
+            CalendarWeekRule myCWR = myCI.DateTimeFormat.CalendarWeekRule;
+            DayOfWeek myFirstDOW = myCI.DateTimeFormat.FirstDayOfWeek;
+            return myCal.GetWeekOfYear(DateTime.Now, myCWR, myFirstDOW);
+        }
+    }
+}
