55a9c59efce86e7cc706461ec7ce8ea15ed5996b
Janik Ahlers <janik.ahlers@outlook.de>
2022-03-24 18:30:15 +0100

    Reduced Apk-size

--- HaspelPlan/HaspelPlan/HaspelPlan.Android/Properties/AssemblyInfo.cs
+++ HaspelPlan/HaspelPlan/HaspelPlan.Android/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using Android.App;
 
@@ -22,10 +21,9 @@ using Android.App;
 //      Minor Version 
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
 
 // Add some common permissions, these can be removed if not needed
 [assembly: UsesPermission(Android.Manifest.Permission.Internet)]
 [assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage)]
-[assembly: UsesPermission(Android.Manifest.Permission.ReadExternalStorage)]
